0edc83ea23063e0ea278b9a317e39ae553566215
StellaIbeh <a.ibeh@alustudent.com>
2024-11-06 17:35:59 +0200

    Connecting to the mongodb database

--- mongodb/mongodb.py
+++ mongodb/mongodb.py
@@ -5,3 +5,7 @@ from pymongo import MongoClient
 file_path = './train.csv'
 dataset = pd.read_csv(file_path)
 
+# Connect to MongoDB
+client = MongoClient("mongodb+srv://aibeh:Parity2017@cluster0.nekvn.mongodb.net/?")
+db = client['sales_database']
+
